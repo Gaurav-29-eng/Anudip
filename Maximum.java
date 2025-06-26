0fba6cb4a1db7cf05f9379e30d591a23cc47ddf9
Gaurav-29-eng <salunkegaurav2916@gmail.com>
2025-06-26 04:36:44 -0400

    Create Maximum.java

---
+++ Maximum.java
@@ -0,0 +1,19 @@
+public class Maximum {
+    public static void main (String[] args){
+        int a = 2;
+        int b = 6;
+        int c = 8;
+
+        if(a >= b & a >= c){
+                    System.out.print("Maximum number is :"+a);
+
+            
+        }
+        else if(b >= a & b >= c){        
+            System.out.print("Maximum number is :"+b);
+}
+        else
+        System.out.print("Maximum number is :"+c);
+    
+}
+}
